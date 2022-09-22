06e40aff03a57cad4015b4906db1dedd3325c893
aanthonyrizzo <arizzo@coreweave.com>
2022-09-22 13:40:45 -0400

    fix(deploy): Add query logic for LATs

--- dashboard/src/components/DeploymentForm/DeploymentForm.tsx
+++ dashboard/src/components/DeploymentForm/DeploymentForm.tsx
@@ -4,6 +4,7 @@ import ChartHeader from "components/ChartView/ChartHeader";
 import Alert from "components/js/Alert";
 import Column from "components/js/Column";
 import Row from "components/js/Row";
+import Button from "components/js/Button";
 import { push } from "connected-react-router";
 import { AvailablePackageReference } from "gen/kubeappsapis/core/packages/v1alpha1/packages";
 import { Plugin } from "gen/kubeappsapis/core/plugins/v1alpha1/plugins";
@@ -60,6 +61,8 @@ export default function DeploymentForm() {
     selected.availablePackageDetail?.availablePackageRef?.plugin ??
       ({ name: pluginName, version: pluginVersion } as Plugin),
   );
+  const [canDeploy, setCanDeploy] = useState(false);
+  const [hasCheckedPermission, setHasCheckedPermission] = useState(false);
 
   useEffect(() => {
     dispatch(
@@ -101,6 +104,71 @@ export default function DeploymentForm() {
   const handleReleaseNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setReleaseName(e.target.value);
   };
+
+  function getCookie(name: any) {
+    const value = `; ${document.cookie}`;
+    const parts = value?.split(`; ${name}=`);
+    if (parts && parts.length === 2) {
+      return parts.pop()?.split(";").shift();
+    } else {
+      return null;
+    }
+  }
+
+  const cloudDomain = window.location.origin.replace(/apps/g, "cloud");
+  useEffect(() => {
+    fetch(`${cloudDomain}/api/`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "x-csrftoken": getCookie("csrftoken") || getCookie("csrftoken_sta") || "",
+      },
+      body: JSON.stringify({
+        query: `
+        query CurrentUser {
+          currentUser {
+            ... on User {
+              id
+              email
+              hasPassword
+              mfaEnabled
+              firstName
+              lastName
+              organization {
+                id
+                subscribed
+                displayName
+              }
+              permissions
+            }
+            ... on Errors {
+              code
+              errors {
+                message
+                path
+              }
+            }
+          }
+        }
+      `,
+      }),
+    })
+      .then(res => res.json())
+      .then(result => {
+        const userPermissions = result.data.currentUser.permissions;
+        if (
+          userPermissions.includes(`w:ns-${namespace}:full`) ||
+          (userPermissions.includes(`w:ns-${namespace}:virtualservers`) &&
+            availablePackageDetail?.name === "virtual-server")
+        ) {
+          setCanDeploy(true);
+        } else {
+          setCanDeploy(false);
+        }
+        setHasCheckedPermission(true);
+      });
+  });
 
   const handleDeploy = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -172,42 +240,65 @@ export default function DeploymentForm() {
           Hang tight, the application is being deployed...
         </h3>
       )}
-      <LoadingWrapper loaded={!isDeploying}>
+      <LoadingWrapper loaded={!isDeploying && hasCheckedPermission}>
         <Row>
           <Column span={3}>
             <AvailablePackageDetailExcerpt pkg={availablePackageDetail} />
           </Column>
           <Column span={9}>
-            {error && <Alert theme="danger">An error occurred: {error.message}</Alert>}
-            <form onSubmit={handleDeploy}>
-              <div>
-                <label
-                  htmlFor="releaseName"
-                  className="deployment-form-label deployment-form-label-text-param"
+            {!canDeploy && hasCheckedPermission ? (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  padding: "80px 200px",
+                }}
+              >
+                <h2
+                  style={{
+                    textAlign: "center",
+                    paddingBottom: "20px",
+                  }}
                 >
-                  Name
-                </label>
-                <input
-                  id="releaseName"
-                  pattern="[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*"
-                  title="Use lower case alphanumeric characters, '-' or '.'"
-                  className="clr-input deployment-form-text-input"
-                  onChange={handleReleaseNameChange}
-                  value={releaseName}
-                  required={true}
-                />
+                  {`You do not have access to our ${availablePackageDetail?.name} application 😞. Please reach out to our support team to get your account upgraded.`}
+                </h2>
+                <Button externalLink={`${cloudDomain}/contact`}>Request permission upgrade</Button>
               </div>
-              <DeploymentFormBody
-                deploymentEvent="install"
-                packageId={packageId}
-                chartVersion={chartVersion}
-                chartsIsFetching={chartsIsFetching}
-                selected={selected}
-                setValues={handleValuesChange}
-                appValues={appValues}
-                setValuesModified={setValuesModifiedTrue}
-              />
-            </form>
+            ) : (
+              <>
+                {error && <Alert theme="danger">An error occurred: {error.message}</Alert>}
+                <form onSubmit={handleDeploy}>
+                  <div>
+                    <label
+                      htmlFor="releaseName"
+                      className="deployment-form-label deployment-form-label-text-param"
+                    >
+                      Name
+                    </label>
+                    <input
+                      id="releaseName"
+                      pattern="[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*"
+                      title="Use lower case alphanumeric characters, '-' or '.'"
+                      className="clr-input deployment-form-text-input"
+                      onChange={handleReleaseNameChange}
+                      value={releaseName}
+                      required={true}
+                    />
+                  </div>
+                  <DeploymentFormBody
+                    deploymentEvent="install"
+                    packageId={packageId}
+                    chartVersion={chartVersion}
+                    chartsIsFetching={chartsIsFetching}
+                    selected={selected}
+                    setValues={handleValuesChange}
+                    appValues={appValues}
+                    setValuesModified={setValuesModifiedTrue}
+                  />
+                </form>
+              </>
+            )}
           </Column>
         </Row>
       </LoadingWrapper>
